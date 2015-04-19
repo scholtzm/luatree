d1dbb02880601bb0ec65b0f69c21e3f968971774
Michael Scholtz <michael.scholtz@outlook.com>
2015-04-19 17:26:18 +0200

    Added one more test.

--- src/luatree/spec/bridge_spec.lua
+++ src/luatree/spec/bridge_spec.lua
@@ -39,7 +39,7 @@ describe("bridge submodule", function()
         assert.is.truthy(t.luadbgraph)
     end)
 
-    it("merges luadb graph data", function()
+    it("merges luadb graph data and creates new nodes", function()
         local t = ast.get_tree(code)
         local g = graph.get_graph(code)
 
@@ -53,5 +53,21 @@ describe("bridge submodule", function()
 
         assert.is.equal(3, calls)
         assert.is.equal(1, eGlobalFunctions)
+    end)
+
+    it("merges luadb graph data and does not create new nodes", function()
+        local t = ast.get_tree(code)
+        local g = graph.get_graph(code)
+
+        bridge.merge_graph_into_AST(t, g, false)
+
+        assert.is.truthy(t)
+        assert.is.truthy(g)
+
+        local calls = utils.count_all_hypergraph_calls(t)
+        local eGlobalFunctions = utils.count_table(utils.get_hypergraph_nodes_by_label(t, "eGlobalFunction"))
+
+        assert.is.equal(3, calls)
+        assert.is.equal(0, eGlobalFunctions)
     end)    
 end)
